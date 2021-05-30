7111ea1b49ad40e6830e3e251df138d1385817bb
Jacob Gordon <62991719+jpgordon00@users.noreply.github.com>
2021-05-30 01:33:18 -0700

    Create CloudscriptHandlers.js

---
+++ CloudscriptHandlers.js
@@ -0,0 +1,42 @@
+handlers.ExecuteFunctionPassthrough = function (args, context) {
+    var entityToken = GetEntityToken("somesecretkeyhere)
+    var headers = {
+        "X-EntityToken": entityToken
+    }
+    
+    var body = {
+        FunctionParameter : args.FunctionParameter,
+        Entity : context.currentEntity,
+        FunctionName : args.FunctionName
+    }
+    var title = "valid_title
+    var url = "https://" + title + ".playfabapi.com/CloudScript/ExecuteFunction"
+    var content = JSON.stringify(body);
+    var httpMethod = "post";
+    var contentType = "application/json";
+
+
+    // The pre-defined http object makes synchronous HTTP requests
+    var response = http.request(url, httpMethod, content, contentType, headers);
+    return { responseContent: response };
+};
+
+function GetEntityToken(secretKey) {
+    var headers = {
+        "X-SecretKey": secretKey
+    }
+    
+    var body = {
+    }
+    var url = "https://E2054.playfabapi.com/Authentication/GetEntityToken"
+    var content = JSON.stringify(body);
+    var httpMethod = "post";
+    var contentType = "application/json";
+
+
+    // The pre-defined http object makes synchronous HTTP requests
+    var response = http.request(url, httpMethod, content, contentType, headers)
+
+    
+    return JSON.parse(response).data.EntityToken
+}
